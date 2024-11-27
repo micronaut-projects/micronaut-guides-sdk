59de7648cfe40a0134cdb3808d996958eada55b5
Sergio del Amo <sergio.delamo@softamo.com>
2024-11-27 10:11:18 +0100

    use attributes

--- guides-cli/src/main/java/io/micronaut/guides/cli/GradleMavenCommandMacro.java
+++ guides-cli/src/main/java/io/micronaut/guides/cli/GradleMavenCommandMacro.java
@@ -6,33 +6,30 @@ import io.micronaut.guides.core.GuidesOption;
 import io.micronaut.guides.core.GuidesTemplatesConfiguration;
 import io.micronaut.guides.core.MacroSubstitution;
 import io.micronaut.guides.core.asciidoc.AsciidocMacro;
+import io.micronaut.guides.core.asciidoc.Attribute;
 import jakarta.inject.Singleton;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Singleton
 public class GradleMavenCommandMacro extends GradleMavenTabs implements MacroSubstitution {
     public static final String MACRO = "gradle-maven-command";
     public static final String START = "<code class=\"language-bash hljs\" data-highlighted=\"yes\"  style=\"white-space: pre-line;\">";
     public static final String END = "</code>";
-    public static final String GRADLE_REGEX = "gradle=([^,]+(?:,[^,=]+)*),maven=";
-    public static final String MAVEN_REGEX = "maven=([^\\]]+)";
+    public static final String ATTRIBUTE_GRADLE = "gradle";
+    public static final String ATTRIBUTE_MAVEN = "maven";
 
-    GradleMavenCommandMacro(ResourceLoader resourceLoader, GuidesTemplatesConfiguration guidesTemplatesConfiguration) {
+    GradleMavenCommandMacro(ResourceLoader resourceLoader,
+                            GuidesTemplatesConfiguration guidesTemplatesConfiguration) {
         super(resourceLoader, guidesTemplatesConfiguration);
     }
 
     @Override
     protected String gradle(AsciidocMacro asciidocMacro, Guide guide, GuidesOption option) {
-        String gradleValue = extractValue(asciidocMacro.raw(), GRADLE_REGEX);
-        return START + gradleValue + END;
+        return attributeValue(asciidocMacro, ATTRIBUTE_GRADLE);
     }
 
     @Override
     protected String maven(AsciidocMacro asciidocMacro, Guide guide, GuidesOption option) {
-        String mavenValue = extractValue(asciidocMacro.raw(), MAVEN_REGEX);
-        return START + mavenValue + END;
+        return attributeValue(asciidocMacro, ATTRIBUTE_MAVEN);
     }
 
     @Override
@@ -40,12 +37,15 @@ public class GradleMavenCommandMacro extends GradleMavenTabs implements MacroSub
         return MACRO;
     }
 
-    private static String extractValue(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        if (matcher.find()) {
-            return matcher.group(1);
+    private String attributeValue(AsciidocMacro asciidocMacro, String attributeKey) {
+        Attribute attribute = asciidocMacro.attributes()
+                .stream()
+                .filter(att -> att.key().equals(attributeKey))
+                .findFirst()
+                .orElse(null);
+        if (attribute == null) {
+            return "";
         }
-        return null;
+        return START + String.join(" ", attribute.values()) + END;
     }
 }
