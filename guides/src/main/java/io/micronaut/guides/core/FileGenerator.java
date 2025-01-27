1529dfb7c8be395c8b328d2d0e04cc1e5933a4e3
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2025-01-27 12:19:19 +0100

    adds FileGenerator class

---
+++ guides/src/main/java/io/micronaut/guides/core/FileGenerator.java
@@ -0,0 +1,23 @@
+package io.micronaut.guides.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public interface FileGenerator {
+
+    default void saveFile(String content, File outputDirectory, String filename) throws IOException {
+        saveFile(content, outputDirectory, filename, false);
+    }
+
+    default void saveFile(String content, File outputDirectory, String filename, boolean executable) throws IOException {
+        Path filePath = Paths.get(outputDirectory.getAbsolutePath(), filename);
+        Files.write(filePath, content.getBytes());
+        if (executable) {
+            filePath.toFile().setExecutable(true);
+        }
+    }
+
+}
