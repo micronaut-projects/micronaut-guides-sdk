8faefcc7ea409b704cf7fb4097e7cd72ea4b676a
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2025-01-28 15:57:06 +0100

    re-enables test

--- guides/src/test/java/io/micronaut/guides/core/GuideProjectZipperTest.java
+++ guides/src/test/java/io/micronaut/guides/core/GuideProjectZipperTest.java
@@ -5,12 +5,13 @@ import io.micronaut.starter.options.BuildTool;
 import io.micronaut.starter.options.Language;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,22 +29,44 @@ public class GuideProjectZipperTest {
     @Inject
     GuideParser guideParser;
 
-    //TODO: fix by zipping an actual guide
-    @Test
-    @Disabled
-    void testZip() throws IOException {
-        String projectDir = "src/test/resources/";
-        String outputDir = "build/tmp/test";
+    @Inject
+    GuideProjectGenerator guideProjectGenerator;
 
-        Guide guide = guideParser.parseGuidesMetadata(new File(projectDir)).stream().filter(g -> g.getSlug().equals("creating-your-first-micronaut-app")).findFirst().get();
+    @Test
+    void testZip() throws IOException {
+        File outputDirectory = Files.createTempDirectory("micronaut-guides").toFile();
+
+        List<Guide> metadatas = new ArrayList<>();
+
+        String path = "src/test/resources/other-guides/adding-commit-info";
+        File file = new File(path);
+        guideParser.parseGuideMetadata(file, "metadata.json").ifPresent(metadatas::add);
+
+        guideProjectGenerator.generate(outputDirectory, metadatas.get(0));
+
         GuidesOption guidesOption = new GuidesOption(BuildTool.GRADLE, Language.JAVA, TestFramework.JUNIT);
 
-        guideProjectZipper.zipGuide(guide, new File(outputDir), new File(outputDir));
+        guideProjectZipper.zipGuide(metadatas.get(0), outputDirectory, outputDirectory);
 
-        List<String> expected = List.of("creating-your-first-micronaut-app.adoc", "metadata.json");
+        List<String> expected = List.of(
+                ".gitignore",
+                "README.md",
+                "build.gradle",
+                "gradle.properties",
+                "gradle/wrapper/gradle-wrapper.jar",
+                "gradle/wrapper/gradle-wrapper.properties",
+                "gradlew",
+                "gradlew.bat",
+                "micronaut-cli.yml",
+                "settings.gradle",
+                "src/main/java/example/micronaut/Application.java",
+                "src/main/resources/application.yml",
+                "src/main/resources/logback.xml",
+                "src/test/java/example/micronaut/DefaultTest.java"
+        );
         List<String> result = new LinkedList<>();
 
-        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(new File(outputDir, guideProjectZipper.getZipFileName(guide, guidesOption))))) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(new File(outputDirectory, guideProjectZipper.getZipFileName(metadatas.get(0), guidesOption) + ".zip")))) {
             ZipEntry zipEntry = zis.getNextEntry();
             while (zipEntry != null) {
                 result.add(zipEntry.getName());
