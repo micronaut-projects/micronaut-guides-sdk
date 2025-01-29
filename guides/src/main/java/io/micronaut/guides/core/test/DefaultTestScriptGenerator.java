5e7342dbf6348334bba2f0d273a1c38613c10638
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2025-01-29 12:42:10 +0100

    missing import

--- guides/src/main/java/io/micronaut/guides/core/test/DefaultTestScriptGenerator.java
+++ guides/src/main/java/io/micronaut/guides/core/test/DefaultTestScriptGenerator.java
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
